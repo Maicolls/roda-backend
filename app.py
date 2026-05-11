010866f9cb0af82aa4ac0b231205b765b8df2b3e
Maicolls <maickgutierrez13@gmail.com>
2026-05-10 19:36:50 -0500

    MOD: se ajustan archivos para el despliegue a productivo

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from routes.simulate import simulate_bp
 from routes.requests import requests_bp
 from database import create_tables
-
+import os
+ 
 app = Flask(__name__)
 CORS(app)
 
@@ -12,4 +13,5 @@ app.register_blueprint(requests_bp)
 
 if __name__ == "__main__":
     create_tables()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
